d62df72cbfa560ff802bbc28d570138535c07805
Emily Cheng <echeng0123@gmail.com>
2023-08-17 16:15:52 -0400

    working button just for safety

--- src/components/SeeDetails.jsx
+++ src/components/SeeDetails.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import SinglePlayer from "./SinglePlayer";
 
+// selectedPuppyId & setSelectedPuppyId on line 5 are pulled in from line 33 on AllPlayers (inside the component call)
 export default function SeeDetails({selectedPuppyId, setSelectedPuppyId}) {
     const [isOpen, setIsOpen] = useState(false);
 
     function handleClick() {
         setIsOpen(!isOpen);
     }
+
     return (
         <div>
             <button onClick={handleClick}>See Details</button>
